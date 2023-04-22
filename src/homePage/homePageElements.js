32420e23633ccf3b8bc53a3f982d0b2523ddc597
Javier Sanchez <48665751+AnthraciteData@users.noreply.github.com>
2023-04-21 18:51:25 -0700

    Changes to HomePage

--- src/homePage/homePageElements.js
+++ src/homePage/homePageElements.js
@@ -18,7 +18,7 @@ cancelBtn.onclick = () => {
 }
 
 window.onscroll = () => {
-    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+  window.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
 
 window.toStudySpace = function () {
